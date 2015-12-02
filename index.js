cd1af89947bc856e1fc637fecf20be3285470e43
wuhy <sparklewhy@gmail.com>
2015-12-02 21:02:30 +0800

    update pack logic

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 /**
- * @file 静态资源打包插件，基于 fis3-packager-map 插件修改。
- *       用法：
- *       fis.match('*.js', {
- *           packTo: 'pkg/all.js'
- *       });
- *       或者：
- *       fis.set('pack', {
- *          '/pkg/all.js': '*.js'
- *       });
- *
+ * @file 静态资源打包插件，支持 amd 模块打包
  * @author sparklewhy@gmail.com
  */
 
@@ -24,15 +15,14 @@ var packUtil = require('./lib/pack');
  * @param {Context} context 打包上下文
  * @param {Object} parseOpts 解析选项
  * @param {Object} settings 打包设置选项
- * @return {Array.<Object>}
  */
 function initPackInfo(context, parseOpts, settings) {
     // 初始化页面打包信息
-    var packedFiles = packUtil.packPage(context, parseOpts) || [];
+    packUtil.packPage(context, parseOpts);
 
     // 初始化指定的文件打包信息
-    var packTos = settings.packTo;
-    packTos.forEach(function (packItem) {
+    var bundles = settings.bundles || [];
+    bundles.forEach(function (packItem) {
         var options = _.merge({}, packItem);
 
         var load = packItem.load;
@@ -41,11 +31,8 @@ function initPackInfo(context, parseOpts, settings) {
 
         options.host = null;
         options.rawCombines = null;
-        var result = packUtil.createPackItem(context, options);
-        result && packedFiles.push(result);
+        context.addToPackItem(packUtil.createPackItem(options, context));
     });
-
-    return packedFiles;
 }
 
 /**
@@ -60,20 +47,22 @@ module.exports = exports = function (ret, pack, settings, opt) {
     try {
         var context = new Context(ret);
         var parseOpts = _.assign({}, htmlParser.defaultOptions, settings.page);
-        parseOpts.pageFiles = context.findFileByPattern(parseOpts.src || '*.html');
+        parseOpts.pageFiles = context.findFileByPattern(parseOpts.files || '*.html');
         parseOpts.pageFiles.forEach(function (file) {
             htmlParser.initPagePlaceholder(file, context, parseOpts);
         });
 
         // 初始化打包信息
-        var packedFiles = initPackInfo(context, parseOpts, settings);
+        initPackInfo(context, parseOpts, settings);
 
         // 初始化打包的顺序
-        packedFiles = packUtil.orderPackItems(packedFiles);
+        var packedFiles = packUtil.orderPackItems(context.toPackItems);
 
         // 开始真正的打包逻辑
         packedFiles = packedFiles.map(function (item) {
             return packUtil.packStaticFiles(context, item);
+        }).filter(function (item) {
+            return item;
         });
 
         packUtil.outputPackInfo(packedFiles, context, parseOpts, settings);
