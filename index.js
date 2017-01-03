a1b557b8a6c56203606985f39dcba74d4ea5d835
wuhy <sparklewhy@gmail.com>
2017-01-03 13:47:14 +0800

    add extractVendor option support

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ var packUtil = require('./lib/pack');
  *
  * @inner
  * @param {Context} context 打包上下文
- * @param {Object} parseOpts 解析选项
+ * @param {Object} packOpts 打包选项
  * @param {Object} settings 打包设置选项
  */
-function initPackInfo(context, parseOpts, settings) {
+function initPackInfo(context, packOpts, settings) {
     // 初始化页面打包信息
-    packUtil.packPage(context, parseOpts);
+    packUtil.packPage(context, packOpts);
 
     // 初始化指定的文件打包信息
     var bundles = settings.bundles || [];
@@ -26,7 +26,7 @@ function initPackInfo(context, parseOpts, settings) {
         var options = _.merge({}, packItem);
 
         var preLoad = packItem.preLoad;
-        preLoad === true && (preLoad = parseOpts.pageFiles);
+        preLoad === true && (preLoad = packOpts.pageFiles);
         options.preLoad = preLoad;
 
         options.host = null;
@@ -46,10 +46,10 @@ function initPackInfo(context, parseOpts, settings) {
 module.exports = exports = function (ret, pack, settings, opt) {
     try {
         var context = new Context(ret);
-        var parseOpts = _.assign({}, htmlParser.getDefaultOption(), settings.page);
-        parseOpts.pageFiles = context.findFileByPattern(parseOpts.files || '*.html');
-        parseOpts.pageFiles.forEach(function (file) {
-            htmlParser.initPagePlaceholder(file, context, parseOpts);
+        var pagePackOpts = _.assign({}, htmlParser.getDefaultOption(), settings.page);
+        pagePackOpts.pageFiles = context.findFileByPattern(pagePackOpts.files || '*.html');
+        pagePackOpts.pageFiles.forEach(function (file) {
+            htmlParser.initPagePlaceholder(file, context, pagePackOpts);
         });
 
         if (typeof settings.preprocessor === 'function') {
@@ -57,19 +57,27 @@ module.exports = exports = function (ret, pack, settings, opt) {
         }
 
         // 初始化打包信息
-        initPackInfo(context, parseOpts, settings);
+        initPackInfo(context, pagePackOpts, settings);
 
         // 初始化打包的顺序
         var packedFiles = packUtil.orderPackItems(context.toPackItems);
 
+        // 初始化打包的文件
+        packedFiles.forEach(function (item) {
+            packUtil.initPackFiles(context, item);
+        });
+        packUtil.extractVendorFiles(
+            packedFiles, context, pagePackOpts
+        );
+
         // 开始真正的打包逻辑
-        packedFiles = packedFiles.map(function (item) {
+        packedFiles = context.toPackItems.map(function (item) {
             return packUtil.packStaticFiles(context, item);
         }).filter(function (item) {
             return item;
         });
 
-        packUtil.outputPackInfo(packedFiles, context, parseOpts, settings);
+        packUtil.outputPackInfo(packedFiles, context, pagePackOpts, settings);
     }
     catch (ex) {
         fis.log.error(ex.stack);
